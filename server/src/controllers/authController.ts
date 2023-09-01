af9fec5685239ec1ea2f23d54f1a4512d1c5fd98
SebAguzzi <107961219+SebAguzzi@users.noreply.github.com>
2023-09-01 09:48:03 -0300

    ruta y controlador del login

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -24,3 +24,31 @@ export const register = async (req: Request, res: Response) => {
     return res.status(500).json({ message: 'Ha ocurrido un error en el servidor.' });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Por favor, rellene todos los campos.' });
+    }
+
+    const existingUser = await User.findOne({ where: { username } });
+
+    if (!existingUser) {
+      return res.status(400).json({ message: 'El nombre de usuario no existe.' });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'La contraseña es incorrecta.' });
+    }
+    
+    return res.status(200).json({ message: 'Login exitoso.' });
+
+  } catch (error) {
+    console.error('Error en el login:', error);
+    return res.status(500).json({ message: 'Ha ocurrido un error en el servidor.' });      
+  }
+}
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { register } from '../controllers/authController';
+import { register, login } from '../controllers/authController';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
+authRouter.post('/login', login);
+
 
 export default authRouter;
