95bef1c2e880db206ec1bfd2cbb621a2b0308fac
SebAguzzi <107961219+SebAguzzi@users.noreply.github.com>
2023-09-08 10:59:14 -0300

    variables

--- server/src/db.ts
+++ server/src/db.ts
@@ -9,18 +9,13 @@ const dbPassword = process.env.DB_PASSWORD;
 const PRODUCTION = process.env.PRODUCTION;
 const DB_DEPLOY = process.env.DB_DEPLOY;
 
-if (PRODUCTION !== 'true' && !dbName || !dbUser || !dbPassword) {
-  throw new Error(
-    "Las variables de entorno de la base de datos no están configuradas."
-  );
-}
 
 const sequelize =
   PRODUCTION === "true"
     ? new Sequelize(DB_DEPLOY || "", {
         dialect: "postgres",
       })
-    : new Sequelize(dbName!, dbUser, dbPassword, {
+    : new Sequelize(dbName!, dbUser!, dbPassword!, {
         host: "localhost",
         dialect: "postgres",
         port: 5432,
